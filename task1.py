c931b0bfcc3b52be87a16c3f9fa625a97fd6f5b0
baklan <baklanvladislav@yandex.ru>
2024-02-21 19:45:40 +0300

    task1.2

--- task1.py
+++ task1.py
@@ -1,9 +1,13 @@
+# 1
 def greatest_common_divisor(a, b):
     while b != 0:
         a, b = b, a % b
     return a
+
+
 def is_coprime(x, y):
     return greatest_common_divisor(x, y) == 1
+
 
 def count_coprime_numbers(n):
     count = 0
@@ -11,3 +15,14 @@ def count_coprime_numbers(n):
         if is_coprime(n, i):
             count += 1
     return count
+
+
+# 2
+def sum_digits_divisible_by_three(number):
+    total = 0
+    while number > 0:
+        digit = number % 10
+        if digit % 3 == 0:
+            total += digit
+        number //= 10
+    return total
