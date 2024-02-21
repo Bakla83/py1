16df3bfddf50916e541fd231efccc99b3f7187da
baklan <baklanvladislav@yandex.ru>
2024-02-21 20:00:44 +0300

    task1.3

--- task1.py
+++ task1.py
@@ -26,3 +26,19 @@ def sum_digits_divisible_by_three(number):
             total += digit
         number //= 10
     return total
+
+
+# 3
+def find_divisor_with_max_coprime_digits(number):
+    max_coprime_count = 0
+    max_coprime_divisor = 1
+    for i in range(2, number + 1):
+        if number % i == 0:
+            coprime_count = 0
+            for digit in str(number):
+                if is_coprime(int(digit), i):
+                    coprime_count += 1
+            if coprime_count > max_coprime_count:
+                max_coprime_count = coprime_count
+                max_coprime_divisor = i
+    return max_coprime_divisor
