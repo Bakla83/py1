ba6062f65911e4fef9bc8c5986dba3a37ec3c501
baklan <baklanvladislav@yandex.ru>
2024-02-21 20:47:22 +0300

    task10

--- task2-4.py
+++ task2-4.py
@@ -1,6 +1,14 @@
-def sort_checker(text):
+'''def sort_checker(text):
     sorted_text = ''.join(sorted(text))
     return sorted_text == text
 
-print(sort_checker('acbaa'))
 
+print(sort_checker('acbaa'))'''
+
+
+def count_letter_A(s):
+    return s.count("A")
+
+
+string = "AbAcAda"
+print("Количество букв 'A' в строке:", count_letter_A(string))
